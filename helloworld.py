724e728feed33e87978da53c94535e256cde838f
ruwaizrazak <r1028rocks@gmail.com>
2015-08-14 11:51:49 +0530

    Day one upto 1:11 hours

--- helloworld.py
+++ helloworld.py
@@ -4,4 +4,12 @@ print ("""hey jude dont make it sad
 	take a sad song 
 	         and make it better.
 
-	         			remember""")
+	         			remember""")\
+
+print(" here is a single quote '"+'ere is a double quote "')
+#if you want a symbol just add a \ in prior to that
+print(" hey here is a \+")
+#any symbol can go in
+#but how about a \?
+print("hey here is a \\")
+#voila, that works
