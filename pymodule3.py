cc26e1bf2ae508cd141fcb74dd4c03ed099a6902
ruwaizrazak <r1028rocks@gmail.com>
2015-08-24 12:15:11 +0530

    adds converting to upper case and lower case

--- pymodule3.py
+++ pymodule3.py
@@ -4,3 +4,10 @@ firstname = input("\nhey, what's your first name?\n")
 lastname = input("last name please\n")
 print ("Heloo " + firstname + " " + lastname)
 
+#converting cases, like upper case, lower case and even swapping too
+
+message = "Hello world"
+print(message.lower())
+print(message.upper())
+print(message.swapcase())
+
