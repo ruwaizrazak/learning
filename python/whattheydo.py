25f84f630dd23def4d4a3f9520899eed63a9a668
ruwaizrazak <r1028rocks@gmail.com>
2015-09-06 18:00:58 +0530

    may be its a cpython conflict, but this is how it works

--- python/whattheydo.py
+++ python/whattheydo.py
@@ -1,7 +1,15 @@
+
 #our duty is to find what they do?
 message='Hello world'
-print(message.count(o)) #counts number of zeroes
-print(message.replace('hello','hi') #replaces hello with hi
-print(message.find('world'))  #find world
-print(message.capitalize()) #capitalizr the first letter of the word
+i=message.count('o') 
+#counts number of zeroes
+j=message.replace('hello','hi')
+#replaces hello with hi
+k=message.capitalize()
+l=message.find('world')
+print(i)
+print(j)
+print(k)
+print(l)
+
 
