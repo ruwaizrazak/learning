3da23a049717bd3477a8e1f46911eeaf6f5b9bba
ruwaizrazak <r1028rocks@gmail.com>
2015-09-06 17:42:12 +0530

    to check what these functions do. the answer is there as comments

--- python/whattheydo.py
+++ python/whattheydo.py
@@ -2,6 +2,6 @@
 message='Hello world'
 print(message.count(o)) #counts number of zeroes
 print(message.replace('hello','hi') #replaces hello with hi
-print(message.find('world')  #find world
+print(message.find('world'))  #find world
 print(message.capitalize()) #capitalizr the first letter of the word
 
