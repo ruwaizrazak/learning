ea902cfff9e483a7eff1d47ed02292881191895f
ruwaizrazak <r1028rocks@gmail.com>
2015-09-13 02:48:43 +0530

    We used the .format for adding numbers

--- python/math1.py
+++ python/math1.py
@@ -8,3 +8,5 @@ print ("the area is %.2f" %area)
 # Why are we using %.2f ? 
 #the %F is the same in the cpp language, the %2.f denotes that we can display up to two decimal places
 #why the print('area =' + area) dont work? because string and numerics wont catenate.
+#Lets fdo the same thing using .format to test it
+print("the area is {0:d}".format(area))
