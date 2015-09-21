c7cb4023156e14eb5cca661474f0a0350bff9f46
ruwaizrazak <r1028rocks@gmail.com>
2015-09-21 15:56:04 +0530

    comments added

--- python/sumofelementsinlist.py
+++ python/sumofelementsinlist.py
@@ -1,4 +1,4 @@
 from array import *
-myarray=array('i',[0,1,2,3,4,5,6,7,8])
-x=sum(myarray)
+myarray=array('i',[0,1,2,3,4,5,6,7,8]) #we are initializing array
+x=sum(myarray) #sum() is a function to add the array.
 print(x)
